dc9eee4944f0f54723a06eae0f224328afd5b5cd
francozanini <francozanini1995@gmail.com>
2019-05-24 20:14:38 -0300

    Updates

---
+++ precache-manifest.e4bf724bd20b491bf7d34168f6d7b6fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53977c03d5a68e2cd9636d60c1b9268",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "0fa152ccf173539d3879",
+    "url": "/game-of-life/static/css/main.6930102f.chunk.css"
+  },
+  {
+    "revision": "4bd1d4eea210ddd5bfad",
+    "url": "/game-of-life/static/js/2.ea93b2ed.chunk.js"
+  },
+  {
+    "revision": "0fa152ccf173539d3879",
+    "url": "/game-of-life/static/js/main.0379a7e3.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  }
+]);
